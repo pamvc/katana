8d394c5cf4e6de7532e5bf84c6fe04a071e3ebde
pamvazquez <pamvazquez@daito.katanagraph.com>
2021-01-12 19:03:36 +0000

    Deleted redundant methods and rearranged format of methods, check parameters!

--- python/galois/lonestar/analytics/average_neighbor_degree.py
+++ python/galois/lonestar/analytics/average_neighbor_degree.py
@@ -7,39 +7,30 @@ from galois.property_graph import PropertyGraph
 from galois.shmem import setActiveThreads
 
 #QUESTIONS: what is the legal way to get value from certain index of an array in pyarrow?
+#           what is the legal way to pass parameters? 
 
-#method that sums up the degrees of the current node's neighbors
-def sum_neighbor_deg(graph, nid, result_dict, deg_array):
 
-    sum_neighbor_degree = 0
-
-    #for each edge connected to curr node: 
-    for edge in graph.edges(nid):
-        #get destination node
-        dst = graph.get_edge_dst(edge)
-        sum += deg_array[nid]
-        steal = True
-        
-    avg_neighbor_degree: sum_neighbor_degree / deg_array[nid]
-    result_dict[nid] = avg_neighbor_degree
-
-##method that sums up the weighted degrees of the current node's neighbors
-def w_sum_neighbor_degree(graph, nid, resultDict, deg_array)
+##method that sums up the nonweighted or weighted degrees of the current node's neighbors
+def sum_neighbor_degree(graph, nid, result_dict, deg_array, weight):
     # create map that will hold the results key = node id and value = avg_neighbor_deg
-    w_sum_neighbor_degree= 0
+    sum_neighbor_degree = 0
     #for edge connected to curr node: 
     for edge in graph.edges(nid):
         #get destination node 
         dst = graph.get_edge_dst(edge)
 
-        w_sum_neighbor_degree += (deg_array[nid] * getEdgeData(edge))
+        if weight = "None":
+            sum_neighbor_degree += deg_array[nid]
 
-    w_avg_neighbor_degree: w_sum_neighbor_degree() / deg_array[nid]
-    result_dict[nid] = w_avg_neighbor_degree
+        elif: 
+            sum_neighbor_degree += (deg_array[nid] * getEdgeData(edge))
+
+    avg_neighbor_degree: sum_neighbor_degree() / deg_array[nid]
+    result_dict[nid] = avg_neighbor_degree
 
 #helper method that fills the result dictionary where key = nid and value = its average neighbor degree 
 @do_all_operator()
-def non_weighted_helper(graph, nid, deg_array):
+def helper(graph, nid, deg_array, weight):
     # create map that will hold the results key = node id and value = avg_neighbor_deg
     result_dict: dict()
     num_nodes = graph.num_nodes()
@@ -47,28 +38,13 @@ def non_weighted_helper(graph, nid, deg_array):
     do_all(
         range(num_nodes),
 
-        sum_neighbor_deg(graph, nid, result_dict, deg_array),
+        sum_neighbor_deg(graph, nid, result_dict, deg_array, weight),
 
         steal=True,
     )
 
     return resultDict
 
-    
-#helper method that fills the result dictionary where key = nid and value = its weighted average neighbor degree 
-@do_all_operator()
-def weighted_helper(graph, nid, deg_array)
-    # create map that will hold the results key = node id and value = avg_neighbor_deg
-    result_dict: dict()
-    num_nodes = graph.num_nodes()
-    #for each node in graph G 
-    do_all(
-        range(num_nodes),
-        w_avg_neighbor_degree(nid, result_dict, deg_array),
-        steal=True,
-    )
-
-    return resultDict
 
 
 def average_neighbor_degree(graph: PropertyGraph, source, target, nodes, weight):
@@ -80,7 +56,5 @@ def average_neighbor_degree(graph: PropertyGraph, source, target, nodes, weight)
     elif: 
         deg_array = graph.get_node_property(outPop)
 
-    if weight == 'None': 
-        print() list(non_weighted_helper(graph: PropertyGraph, deg_array))
-    else: 
-        print() list(weighted_helper(graph: PropertyGraph), deg_array))
+    print() list(helper(graph: PropertyGraph, deg_array, weight))
+   
