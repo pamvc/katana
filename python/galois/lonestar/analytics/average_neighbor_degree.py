fb1f0e0cce41c2f4e786daf56cea7caf725bba66
pamvazquez <pamvazquez@daito.katanagraph.com>
2021-01-13 19:47:56 +0000

    Parameters for calculate degree

--- python/galois/lonestar/analytics/average_neighbor_degree.py
+++ python/galois/lonestar/analytics/average_neighbor_degree.py
@@ -50,7 +50,7 @@ def helper(graph, nid, deg_array, weight):
 
 def average_neighbor_degree(graph: PropertyGraph, source, target, weight):
    
-    calculate_degree(graph, "in_degree_property", "out_degree_property", weight_property = weight)
+    calculate_degree(graph, in_degree_property="in_degree_property", out_degree_property="out_degree_property", weight_property = weight)
 
     if source == "in" and target == "in":
         deg_array = graph.get_node_property(inPop)
